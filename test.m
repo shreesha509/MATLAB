a8727c4ca2d3d1401bc81b6ca44274b5875ca201
shreesha kumar p <shreeshbhat0509@gmail.com>
2026-07-12 00:41:52 +0530

    Delete test.m

--- test.m
+++
@@ -1,9 +0,0 @@
-[t, yp] = sim_passive();
-[~, ypid] = sim_pid_reactive();
-[~, yai] = sim_ai_predictive();
-
-pid_peak = max(abs(ypid));
-ai_peak = max(abs(yai));
-
-fprintf('PID overall peak: %f\n', pid_peak);
-fprintf('AI overall peak: %f\n', ai_peak);
